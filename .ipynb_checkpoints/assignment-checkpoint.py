4ef1f4d6bf5bb45d88cd20bb521a75566e4c8a05
Zuberi0025 <mabalozy22@gmail.com>
2025-01-18 05:49:00 +0300

    make changes to assignment

--- .ipynb_checkpoints/assignment-checkpoint.py
+++ .ipynb_checkpoints/assignment-checkpoint.py
@@ -1,7 +1,4 @@
 def format_string(name, age):
-    name='Zuberi'
-    age=30
-    print(f'My Name is {name}, am {age} years old')
     """
     Create a formatted string using f-strings.
     Args:
@@ -13,14 +10,6 @@ def format_string(name, age):
     pass
 
 def conditional_check(number):
-    x=5
-    if x>10:
-        print('x is greater')
-    elif x<10:
-        print('x is less')
-    else:
-        print('is equal to 10')
-    
     """
     Check if a number is greater, lesser, or equal to 10.
     Args:
@@ -31,11 +20,6 @@ def conditional_check(number):
     pass
 
 def loop_sum(n):
-    n=int(2)
-    sum=0
-    for i in range(1,n+1):
-        sum+=i
-      
     """
     Calculate sum of numbers from 1 to n using a loop.
     Args:
@@ -46,10 +30,6 @@ def loop_sum(n):
     pass
 
 def list_operations(numbers):
-    list=[1,2,3,5,6,7,8,11,21,8,9,41,6]
-    print(sum(list))
-    print(max(list))
-    print(min(list))
     """
     Perform operations on a list of numbers.
     Args:
@@ -60,13 +40,6 @@ def list_operations(numbers):
     pass
 
 def dict_operations(students_dict):
-    dict_by_func = dict(Jonah = 65,
-                    Siwema = 70,
-                    Chris= 77,
-                    Zuberi = 80,
-                    Abdallah= 97, 
-                    Grace= 100)
-    print(dict_by_func['Zuberi','Abdallah','Grace']) 
     """
     Find students with scores above 80.
     Args:
@@ -77,9 +50,6 @@ def dict_operations(students_dict):
     pass
 
 def set_operations(list1, list2):
-    list1=[1,2,3,"a","z",85]
-    list2=["c",2,5,6,8,"a","t"] #Common element can be found in sets by  intersection operation
-    
     """
     Find common elements between two lists.
     Args:
@@ -91,11 +61,6 @@ def set_operations(list1, list2):
     pass
 
 def arithmetic_ops(a, b):
-    a=7
-    b=9
-    c=a+b
-    d=a-b
-    e=a*b
     """
     Perform arithmetic operations.
     Args:
@@ -107,14 +72,6 @@ def arithmetic_ops(a, b):
     pass
 
 def logical_ops(x, y):
-    x = 3
-    y = 7
-
-    if x > 0 and y > 0:
-        print("Both x and y are positive.")
-    else:
-        print("At least one is not positive.")
-
     """
     Perform logical operations.
     Args:
@@ -126,12 +83,6 @@ def logical_ops(x, y):
     pass
 
 def bitwise_ops(a, b):
-    number = 0
-    if number & 1:  
-        print("Odd")
-    else:
-        print("Even")
-
     """
     Perform bitwise operations.
     Args:
